d91dd670e1d4e7d566be75f117653331bb77832a
buresj <bures@fakultnisociologie.cz>
2019-06-07 10:27:45 +0200

    Pixam: get JSON order by ID added

---
+++ src/main/java/com/greenfox/orientationexam/controllers/APIController.java
@@ -0,0 +1,26 @@
+package com.greenfox.orientationexam.controllers;
+
+
+import com.greenfox.orientationexam.models.Order;
+import com.greenfox.orientationexam.services.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+
+    private OrderService orderService;
+
+    public APIController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/api/orders/{id}")
+    public ResponseEntity getOrder(@PathVariable long id){
+        return new ResponseEntity<>(orderService.findOrderById(id).get(), HttpStatus.ACCEPTED);
+    }
+
+}
